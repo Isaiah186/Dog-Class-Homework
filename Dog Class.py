3b26fea76152d95cffe1e4963bd7edbba7c0993b
Isaiah186 <91351729+Isaiah186@users.noreply.github.com>
2021-10-02 17:30:06 -0500

    Add files via upload

---
+++ Dog Class.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+class Dog:
+    def __init__(self):
+            pass
+    def eat(self):
+        print("The dog is eating")
+        
+    def sleep(self):
+        print("The dog is sleeping")
+        
+    def bark(self):
+        print("The dog is barking")
+        
+    def running(self):
+        print("The dog is running")
+
+
+# In[11]:
+
+
+Gracie_obj=Dog()
+
+
+# In[12]:
+
+
+Gracie_obj.eat()
+
+
+# In[ ]:
+
+
+
+
